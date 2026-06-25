401115d860e018cde120e5bed7a0ca99c5e0547b
roshansalunke-imp <roshan.salunke@impactguru.com>
2026-06-25 15:15:49 +0530

    Delete corrupted Vercel cache to fix backend

--- .vercel/output/static/run_git.js
+++
@@ -1,9 +0,0 @@
-const { execSync } = require('child_process');
-const fs = require('fs');
-
-try {
-  const output = execSync('git status', { cwd: 'c:/Users/6451/Documents/care360/care360', encoding: 'utf-8' });
-  fs.writeFileSync('c:/Users/6451/Documents/care360/care360/git_status_node.txt', output);
-} catch (e) {
-  fs.writeFileSync('c:/Users/6451/Documents/care360/care360/git_status_node.txt', e.toString() + '\n' + (e.stdout ? e.stdout.toString() : ''));
-}
--- .vercel/output/static/status.py
+++
@@ -1,8 +0,0 @@
-import subprocess
-
-with open('git_status_output.txt', 'w') as f:
-    try:
-        output = subprocess.check_output(['git', 'status'], stderr=subprocess.STDOUT)
-        f.write(output.decode('utf-8'))
-    except subprocess.CalledProcessError as e:
-        f.write(e.output.decode('utf-8'))
